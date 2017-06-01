5c2a103b2efddfa6a54b19d89e06b8232b708fed
flor condori <condorihuamanif@gmail.com>
2017-06-01 15:12:01 -0500

    un objeto con tiutlo y url

--- flickr-fetcher.js
+++ flickr-fetcher.js
@@ -1,7 +1,17 @@
 var FlickrFetcher;
 
 FlickrFetcher = {
-    photoObjToURL: function() {}
+	photoObjToURL: function(photoObj) {
+        return [ 'https://farm',
+            photoObj.farm, '.staticflickr.com/', 
+            photoObj.server, '/',
+            photoObj.id, '_',
+            photoObj.secret, '_b.jpg'
+        ].join('');
+    },
+    transformPhotoObj: function(photoObj){
+
+    }
 };
 
 module.exports = FlickrFetcher;
