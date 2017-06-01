ad527a6b0ec7f03a9463d38abd5348c460723ca3
flor condori <condorihuamanif@gmail.com>
2017-06-01 12:18:43 -0500

    agregando el primer test

--- flickr-fetcher-spec.js
+++ flickr-fetcher-spec.js
@@ -1,1 +1,9 @@
-flickr-fetcher-spec
+'use strict';
+var expect = require('chai').expect;
+
+describe('FlickrFetcher', function() {
+    it('should exist', function() {
+        var FlickrFetcher = require('./flickr-fetcher.js');
+        expect(FlickrFetcher).to.not.be.undefined;
+    });
+});
